108b649f15b0e5d64affdc4a69295f33cf20701e
emmanuel <ejariza@gmail.com>
2020-08-08 19:38:54 -0500

    Deleted unnecesary code in dembow pattern

--- tutorial/src/tutorial/patterns/dembow.clj
+++ tutorial/src/tutorial/patterns/dembow.clj
@@ -3,7 +3,6 @@
             [overtone.inst.drum :as o-drums]
             [leipzig.live :as live]
             [leipzig.melody :refer :all]
-            [tutorial.utils :refer [pattern2phrase]]
             )
   )
 
@@ -28,49 +27,3 @@
 (def claps-hats (-> combined-pattern
                     (assoc :close-hat [1 1 1 0 0 0 0 0 1 1 1 0 1 0 1 0])
                     (assoc :clap      [1 0 1 1 0 1 0 1 0 1 1 0 1 0 0 0])))
-
-;; (def dembow-phrase (->> dembow-claps-hats
-;;                               (map (partial pattern2phrase 0.25))
-;;                               (apply with)
-;;                               (all :part :dembow)
-;;                               ))
-
-
-;; (def dembow-pattern (->>
-;;                      {:kick  (take 16 (cycle [1 0 0 0]))
-;;                       :snare (concat [0 0 0 1 0 0 1 0]
-;;                                      [0 0 0 1 0 0 1 1])
-;;                       :close-hat (take 16 (cycle (concat
-;;                                                   [1 1 1 0 0 0 0 0]
-;;                                                   [1 1 1 0 1 0 1 0]
-;;                                                   )))
-;;                       :clap [1 0 1 1 0 1 0 1 0 1 1 0 1]}
-
-;;                      (map (partial pattern2phrase 0.25))
-;;                      (apply with)
-;;                      (all :part :dembow)
-;;                      )
-;;   )
-
-;; (defmethod live/play-note :dembow [note]
-;;   (when-let [drum (-> (get kit (:drum note)))]
-;;     (drum)))
-;; ()
-;; (->> ()
-;;        (tempo (bpm 90))
-;;        live/play)
-
-;; (def dbpt (->> basic-dembow-phrase
-;;                (tempo (bpm 90))
-;;                )
-;;   )
-
-;; (live/jam (var dbpt))
-
-;; (live/stop)
-
-;; (var dbpt)
-
-
-
-;; dembow-double-snare-pattern
